86955c1c551f396967c6ff25ede684978abc5bfc
cppchriscpp <admin@cpprograms.net>
2017-05-23 20:24:44 -0400

    Make web movement better

--- demos/character-movement/server/index.js
+++ demos/character-movement/server/index.js
@@ -2,12 +2,18 @@ var express = require('express'),
 	log = require('winston'),
 	app = express(),
 	port = 3000,
+	DIRECTION_UP = 0,
+	DIRECTION_DOWN = 1,
+	DIRECTION_LEFT = 2,
+	DIRECTION_RIGHT = 3,
 	positionX = 32,
-	positionY = 128;
+	positionY = 128,
+	direction = DIRECTION_DOWN;
 
 function updatePosition(req) {
 	positionX = parseInt(req.params.x, 10);
 	positionY = parseInt(req.params.y, 10);
+	direction = parseInt(req.params.direction, 10);
 
 	if (positionX > 240) 
 		positionX = 0;
@@ -18,27 +24,27 @@ function updatePosition(req) {
 }
 
 app.get('/position.json', function(req, res) {
-	res.json({x: positionX, y: positionY});
+	res.json({x: positionX, y: positionY, direction: direction});
 });
 
 app.get('/position', function(req, res) {
-	res.end(new Buffer([positionX, positionY, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255]));
+	res.end(new Buffer([positionX, positionY, direction, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255]));
 });
 
 
-app.get('/update/:x/:y.json', function(req, res) {
+app.get('/update/:x/:y/:direction.json', function(req, res) {
 	updatePosition(req);
-	res.json({x: positionX, y: positionY});
+	res.json({x: positionX, y: positionY, direction: direction});
 });
 
-app.get('/update/:x/:y', function(req, res) {
+app.get('/update/:x/:y/:direction', function(req, res) {
 	updatePosition(req);
-	res.end(new Buffer([positionX, positionY, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255]));
+	res.end(new Buffer([positionX, positionY, direction, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255]));
 });
 
 
 app.get('/', function(req, res) {
-	res.render('index.pug', {positionX: positionX, positionY: positionY});
+	res.render('index.pug', {positionX: positionX, positionY: positionY, direction: direction});
 });
 app.use('/static', express.static('public'));
 
--- demos/character-movement/server/public/movement.js
+++ demos/character-movement/server/public/movement.js
@@ -1,27 +1,59 @@
 (function() {
 
+	var MOVEMENT_SPEED = 4,
+		DIRECTION_UP = 0,
+		DIRECTION_DOWN = 1,
+		DIRECTION_LEFT = 2,
+		DIRECTION_RIGHT = 3,
+		DIRECTION_LOOKUP = {};
+	
+	DIRECTION_LOOKUP[DIRECTION_UP] = 'up';
+	DIRECTION_LOOKUP[DIRECTION_DOWN] = 'down';
+	DIRECTION_LOOKUP[DIRECTION_LEFT] = 'left';
+	DIRECTION_LOOKUP[DIRECTION_RIGHT] = 'right';
+
 	var positionX = 0,
-		positionY = 0;
+		positionY = 0,
+		direction = DIRECTION_DOWN,
+		moveLock = false;
 
 	function getLocationFromServer() {
 		$.get('/position.json', function(data) {
 			positionX = data.x;
 			positionY = data.y;
+			direction = data.direction;
 			redrawPosition();
 		});
 	}
 
-	function setLocationOnServer(x, y) {
-		$.get('/update/'+x+'/'+y+'.json', function(data) {
+	function setLocationOnServer(x, y, direction) {
+		if (moveLock)
+			return;
+		moveLock = true;
+		$.get('/update/'+x+'/'+y+'/'+direction+'.json', function(data) {
+			moveLock = false;
 			positionX = data.x;
 			positionY = data.y;
+			direction = data.direction;
 			redrawPosition();
 		});
+	}
+
+	function normalizePosition(position) {
+
+		if (position < 0)
+			position = 0;
+		else if (position > 220)
+			position = 220;
+
+		return position;
+
 	}
 
 	function redrawPosition() {
 		$('span.positionX').text(positionX);
 		$('span.positionY').text(positionY);
+		$('span.direction').text(DIRECTION_LOOKUP[direction]);
 		$('.panel.world .character').css({left: (2*positionX), top: (2*positionY)});
 	}
 
@@ -34,7 +66,37 @@
 
 			x = Math.floor(x/2);
 			y = Math.floor(y/2);
-			setLocationOnServer(x, y);
+			x = normalizePosition(x);
+			y = normalizePosition(y);
+			setLocationOnServer(x, y, direction);
+		});
+
+		$('body').keydown(function(event) {
+			switch (event.which) {
+				case 87: // w
+					direction = DIRECTION_UP;
+					positionY -= MOVEMENT_SPEED;
+					break;
+				case 65: // a
+					direction = DIRECTION_LEFT;
+					positionX -= MOVEMENT_SPEED;
+					break;
+				case 83: // s
+					direction = DIRECTION_DOWN;
+					positionY += MOVEMENT_SPEED;
+					break;
+				case 68: // d
+					direction = DIRECTION_RIGHT;
+					positionX += MOVEMENT_SPEED;
+					break;
+
+				default: 
+					return; // Do nothing!
+			}
+			positionX = normalizePosition(positionX);
+			positionY = normalizePosition(positionY);
+
+			setLocationOnServer(positionX, positionY, direction);
 		});
 	});
 })();
